438917ddc160cc387db906f03541a3ed27cec3d4
Ahmedf <ahmedfaisalzayed@gmail.com>
2025-03-02 17:40:54 +0200

    another commit

--- src/user.php
+++ src/user.php
@@ -11,10 +11,16 @@ class User{
     public function login($email,$password){
         $sql = "SELECT *FROM account WHERE email ='$email'";
         $result= mysqli_query($this->db->connect,$sql);
-        if($result)
-            header("location : index.php");
-        else 
-        return "this account is not exist ";
+        if($row=mysqli_fetch_assoc($result)){
+            if(password_verify($password,$row['password'])){
+                return "login is successfull";
+            } else {
+                return "your pass is invalid , check your info";
+            }
+        } else {
+            return "this account is not exist";
+        }
+
     }
 }
 
