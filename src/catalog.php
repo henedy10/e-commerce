c42f8bee785cf5754c04f2e763be53402d67a195
Ahmedf <ahmedfaisalzayed@gmail.com>
2025-03-10 23:30:22 +0200

    another commit

--- src/catalog.php
+++ src/catalog.php
@@ -3,6 +3,7 @@ session_start();
 include "db.php";
 $quantity=1 ;
 $total_price=0;
+$message="";
 $check=new DataBase();
 $connect = $check->connect;
 $sql="SELECT id FROM products";
@@ -26,6 +27,9 @@ if(isset($_POST['add_to_cart'])){
                             VALUES('$email','$quantity','$price_catalog',
                                   '$total_price','$image_catalog','$name_catalog')";
     $result_cart = mysqli_query($connect,$sql_cart);
+    if($result_cart){
+      $message="Addition to your cart is done successfully";
+    }
   }
     
     ?>
@@ -623,29 +627,34 @@ if(isset($_POST['add_to_cart'])){
         <!-- breadcrumbs  -->
         
         <nav class="mx-auto w-full mt-4 max-w-[1200px] px-5">
-          <ul class="flex items-center">
-            <li class="cursor-pointer">
-              <a href="index.php">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="h-5 w-5"
-                  >
-                  <path
-                  d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"
-                  />
-                  <path
-                  d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"
-                  />
-                </svg>
-              </a>
-            </li>
-            <li>
-              <span class="mx-2 text-gray-500">&gt;</span>
-            </li>
-            
-            <li class="text-gray-500">Catalog</li>
+          <ul class="flex justify-between">
+            <div class="flex ">
+              <li class="cursor-pointer">
+                <a href="index.php">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5"
+                    >
+                    <path
+                    d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"
+                    />
+                    <path
+                    d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"
+                    />
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <span class="mx-2 text-gray-500">&gt;</span>
+              </li>
+              
+              <li class="text-gray-500">Catalog</li>
+            </div>
+            <div class=" text-center font-bold w-1/2">
+              <li class="bg-green-300"><?php echo $message ?></li>
+            </div>
           </ul>
         </nav>
         <!-- /breadcrumbs  -->
@@ -1041,16 +1050,16 @@ if(isset($_POST['add_to_cart'])){
                 </div>
                 <form action="<?php $_SERVER["PHP_SELF"]?>" method="post">
                   <div>
-                    <button class="my-5 h-10 w-full font-bold bg-violet-900 hover:bg-violet-700 text-white rounded-lg cursor-pointer" type="submit" name="add_to_cart" value="<?php echo $rows['id'] ?>">
+                    <button class="mt-5 h-10 w-full font-bold bg-violet-900 hover:bg-violet-700 text-white rounded-lg cursor-pointer" type="submit" name="add_to_cart" value="<?php echo $rows['id'] ?>">
                       Add to cart
                     </button>
                   </div>
                 </form>
-                </div>
               </div>
-              <?php endfor; ?>
             </div>
-          </section>
+            <?php endfor; ?>
+          </div>
+        </section>
         </div>
       </section>
 
