bcb742f0def1bf9ff95a1a49185f7c654f927e16
Ahmedf <ahmedfaisalzayed@gmail.com>
2025-03-15 17:02:09 +0200

    another commit

--- src/contact-us.php
+++ src/contact-us.php
@@ -14,7 +14,7 @@ if(isset($_POST['send'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="assets/css/tailwind-ecommerce.css" />
+    <link rel="stylesheet" href="./assets/css/tailwind-ecommerce.css" />
 
     <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
